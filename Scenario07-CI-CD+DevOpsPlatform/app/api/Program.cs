7692ae7e424d47df9dabfa5121b89e9cc4437b62
Dragan Z. <95223276+student-asss@users.noreply.github.com>
2026-02-27 10:47:19 +0100

    Update Program.cs

--- Scenario07-CI-CD+DevOpsPlatform/app/api/Program.cs
+++ Scenario07-CI-CD+DevOpsPlatform/app/api/Program.cs
@@ -1,1 +1,9 @@
 
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "API is running");
+app.MapGet("/health", () => Results.Ok("Healthy"));
+
+app.Run();

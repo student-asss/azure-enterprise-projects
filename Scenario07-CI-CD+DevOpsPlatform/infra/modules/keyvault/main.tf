190d98f552646456f7599a5480ada8d362d4fe4f
Dragan Z. <95223276+student-asss@users.noreply.github.com>
2026-03-01 11:28:41 +0100

    Update main.tf

--- Scenario07-CI-CD+DevOpsPlatform/infra/modules/keyvault/main.tf
+++ Scenario07-CI-CD+DevOpsPlatform/infra/modules/keyvault/main.tf
@@ -1,1 +1,16 @@
+resource "azurerm_key_vault" "kv" {
+  name                        = "kv-s07-devops"
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = false
+  soft_delete_retention_days  = 7
+}
+
+data "azurerm_client_config" "current" {}
+
+output "kv_name" {
+  value = azurerm_key_vault.kv.name
+}
 

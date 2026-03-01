1eec0690e6795f1a94dd056fe8ada10e18c676f7
Dragan Z. <95223276+student-asss@users.noreply.github.com>
2026-03-01 11:29:35 +0100

    Update main.tf

--- Scenario07-CI-CD+DevOpsPlatform/infra/modules/appservice/main.tf
+++ Scenario07-CI-CD+DevOpsPlatform/infra/modules/appservice/main.tf
@@ -1,1 +1,25 @@
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-s07-api"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "app-s07-api"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "8.0"
+    }
+  }
+}
+
+output "app_name" {
+  value = azurerm_linux_web_app.app.name
+}
 

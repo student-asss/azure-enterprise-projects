78d6bccab1830a99a6a8ec0202ab33e54c540ae3
Dragan Z. <95223276+student-asss@users.noreply.github.com>
2026-03-01 11:24:33 +0100

    Update main.tf

--- Scenario07-CI-CD+DevOpsPlatform/infra/main.tf
+++ Scenario07-CI-CD+DevOpsPlatform/infra/main.tf
@@ -1,1 +1,50 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-s07-devops"
+  location = "West Europe"
+}
+
+# Log Analytics
+module "loganalytics" {
+  source              = "./modules/loganalytics"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# Key Vault
+module "keyvault" {
+  source              = "./modules/keyvault"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# App Service + Plan
+module "appservice" {
+  source              = "./modules/appservice"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# DCR + Custom Table
+module "dcr" {
+  source               = "./modules/dcr"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
+  log_analytics_id     = module.loganalytics.workspace_id
+}
 
